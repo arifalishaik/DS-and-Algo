e348614f04e6f65148466913ce4e4705df87594a
arifalishaik <arifali2016@gmail.com>
2018-03-01 17:26:38 +0530

    print the number with give bit_positions set

---
+++ Week 1/bit_position_set.cpp
@@ -0,0 +1,22 @@
+// Author: S M ARIF ALI
+// this program sets the bits of given respective positions and prints the number
+#include <iostream>
+using namespace std;
+
+// setting the bits at given respective positions and returning the number back
+long int get_Number( int a, int b ){
+    if( a == b ){
+        return (1L << a);
+    }
+    return ( 1L << a ) + ( 1L << b );
+}
+
+// main function
+int main( void ){
+    int a, b;
+    // reading the bit positions to be set
+    cin >> a >> b;
+
+    long int num = get_Number( a, b );
+    cout << num << endl;
+}
