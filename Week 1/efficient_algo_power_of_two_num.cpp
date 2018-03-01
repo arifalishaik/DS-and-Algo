37fb0baed83f6ed0ad04d7e45626b4b1fbd22b96
arifalishaik <arifali2016@gmail.com>
2018-03-02 05:27:59 +0530

    idented efficient algo to calculate power of given two numbers

--- Week 1/efficient_algo_power_of_two_num.cpp
+++ Week 1/efficient_algo_power_of_two_num.cpp
@@ -2,8 +2,8 @@
 // This program prints the value for a^b. Both a and b are given as input
 // Since the out put can be large we are using modulo with a very large number 10000000007
 
-// But here is the problem. If a = 2 and b = some very large number like 10^9
-// Therefore, 10^9 iterations by a for loop may result in timeout on most online judges
+// But here is the problem. If a = 2 and b = some very large number like 10^5 or >. 
+// The Time Complexity of your program should not be O(b) as it may often result in timeout of the program while iterating b times. ( here b is > 10^5 )
 // so this Algorithm is not efficient for calulating the power of a given very large number
 
 // So this progam calulates very efficiently
@@ -13,24 +13,20 @@ using namespace std;
 
 // calculating a^b
 long long int power( long long int a, long long int b ){
+    
     long long int power = 1;
-
     while ( b != 0 ){
-
         if( b & 1 ){
             power = ( power * a ) % 1000000007;
         }
-
-        a = ( a * a ) % 1000000007;
+        a = (a * a) % 1000000007;
         b = b >> 1;
     }
-
     cout << power << endl;
 }
 
 // main function
-int main( void )
-{
+int main( void ){
     long long int a, b;
     cin >> a >> b;
     power( a, b );
