1e6401320336d225e8d4105afccbb449d6615794
arifalishaik <arifali2016@gmail.com>
2018-03-02 05:16:18 +0530

    efficient algo to caluculate a^b

--- Week 1/efficient_algo_power_of_two_num.cpp
+++ Week 1/efficient_algo_power_of_two_num.cpp
@@ -2,29 +2,36 @@
 // This program prints the value for a^b. Both a and b are given as input
 // Since the out put can be large we are using modulo with a very large number 10000000007
 
-// But here is the problem. If a = 2 and b = some very large number like 10^9 
+// But here is the problem. If a = 2 and b = some very large number like 10^9
 // Therefore, 10^9 iterations by a for loop may result in timeout on most online judges
 // so this Algorithm is not efficient for calulating the power of a given very large number
+
+// So this progam calulates very efficiently
 
 #include <iostream>
 using namespace std;
 
 // calculating a^b
-int power( int a, int b )
-{
-    long int power = 1;
-    for ( int i = 0; i < b; ++i )
-    {
-        // % is necessary as the out put can be very very large
-        power = ( power * a ) % 10000000007;
+long long int power( long long int a, long long int b ){
+    long long int power = 1;
+
+    while ( b != 0 ){
+
+        if( b & 1 ){
+            power = ( power * a ) % 1000000007;
+        }
+
+        a = ( a * a ) % 1000000007;
+        b = b >> 1;
     }
 
     cout << power << endl;
 }
 
 // main function
-int main( void ){
-    long int a, b;
+int main( void )
+{
+    long long int a, b;
     cin >> a >> b;
     power( a, b );
 }
