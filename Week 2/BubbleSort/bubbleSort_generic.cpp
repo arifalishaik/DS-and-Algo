019790e7f361c81beed291bba2cea80b1beec01c
arifalishaik <arifali2016@gmail.com>
2018-03-05 20:20:38 +0530

    bubble sort

---
+++ Week 2/BubbleSort/bubbleSort_generic.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+void swap( int *a, int *b )
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubbleSort( int *arr, int N ){
+
+    for( int i = 0; i < N - 1; ++i ){
+        // Last i elements are already in place
+        for ( int j = 0; j < N - i - 1; ++j ){
+            
+            if( arr[ j + 1 ] < arr[ j ] ){
+                // swap the two adjacent elements
+                swap( &arr[ j ], &arr[ j + 1 ] );
+            }
+        }
+    }
+}
+
+void printArray( int *arr, int N ){
+    for ( int i = 0; i < N; ++i ){
+        cout << arr[i] << " ";
+    }
+}
+
+int main( void ){
+    // Enter the size of the array N
+    int N;
+    cin >> N;
+
+    // declaring the size of the array N
+    int arr[N];
+    
+    // reading the array elements
+    for ( int i = 0; i < N; ++i ){
+        cin >> arr[i];
+    }
+
+    bubbleSort( arr, N );
+    printArray( arr, N);
+}
+
+/*
+Time Complexity: 
+
+
+*/
